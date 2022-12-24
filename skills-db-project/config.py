8d19b7a121f2caa460fa4474feba2823bef17ba2
Pirate2606 <adityanaitan@gmail.com>
2022-12-24 14:16:32 +0530

    fixed some bugs

--- skills-db-project/config.py
+++ skills-db-project/config.py
@@ -6,7 +6,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = "supersecret" or os.environ.get("FLASK_SECRET_KEY")
-    # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'data.sqlite') or os.environ.get('DATABASE_URL')
-    SQLALCHEMY_DATABASE_URI = 'postgresql://aditya:Supersecret@db:5432/projectB'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'data.sqlite') or os.environ.get('DATABASE_URL')
+    # SQLALCHEMY_DATABASE_URI = 'postgresql://aditya:Supersecret@db:5432/projectB'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     PARENT_FOLDER = 'skills-report-micro-app'
--- skills-db-project/skills-list-micro-app/config.py
+++ skills-db-project/skills-list-micro-app/config.py
@@ -6,6 +6,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = "supersecret" or os.environ.get("FLASK_SECRET_KEY")
-    # SQLALCHEMY_DATABASE_URI = 'sqlite:///F:\\Programs\\Hackathons\\AFourathon_2.0\\skills-db-project\\data.sqlite'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://aditya:Supersecret@db:5432/projectB'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///F:\\Programs\\Hackathons\\AFourathon_2.0\\skills-db-project\\data.sqlite'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql://aditya:Supersecret@db:5432/projectB'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
